a70a09fab532d5d2c084f5d95c9bf7d8d3b52ac7
Basil-NB <120618274+Basil-NB@users.noreply.github.com>
2024-07-02 13:50:00 -0400

    fixed exports to app

---
+++ latest/src/components/index.js
@@ -0,0 +1,13 @@
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Contact,
+  Projects,
+}
