de85495fafbc3dfa0ae68b5d2d8e2eb166db3d7f
Basil-NB <120618274+Basil-NB@users.noreply.github.com>
2024-07-02 14:08:28 -0400

    fixed some imports and all base componets are ready for content and styling

---
+++ latest/src/constants/index.js
@@ -0,0 +1,14 @@
+export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "Projects",
+      title: "Projects",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
